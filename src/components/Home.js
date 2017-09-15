f1b84dc6b2bbe1f4e693264749208da0e6949c8b
zijing <1157292457@qq.com>
2017-09-15 14:42:23 +0800

    测试1111

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Link } from 'react-router-dom'
+import '../homestyle.css'
 
-class Home extends React.Component {
+
+class Home extends React.Component {	
 	render() {
 		return (
 			<div>
-				<div class="swiper-container">
-				    <div class="swiper-wrapper">
-				        <div class="swiper-slide">Slide 1</div>
-				        <div class="swiper-slide">Slide 2</div>
-				        <div class="swiper-slide">Slide 3</div>
-				    </div>
-				    <!-- 如果需要分页器 -->
-				    <div class="swiper-pagination"></div>
-				    
-				    <!-- 如果需要导航按钮 -->
-				    <div class="swiper-button-prev"></div>
-				    <div class="swiper-button-next"></div>
-				    
-				    <!-- 如果需要滚动条 -->
-				    <div class="swiper-scrollbar"></div>
-				</div>
+				
 			</div>
 		)
 	}
+		    
 }
 
 
