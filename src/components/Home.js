2370bab34ccf12b042108282387f2caa3edd4a68
zijing <1157292457@qq.com>
2017-09-15 11:18:35 +0800

    提交试试

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,55 +2,31 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Link } from 'react-router-dom'
 
-class HomeUI extends Component {
-	componentDidMount() {
-		this.props.fetchListData();
-	}
+class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				<h2>首页</h2>
-				<hr/>
-				<h3>商品列表</h3>
-				<ul>
-					{
-						this.props.home_list.map((item, index)=>{
-							return <li key={"h_l_" + index}>
-								<img src={item.logo} alt={'img' + index}/>
-								<span>{item.shop_name}</span>
-								<span>{item.price}</span>
-								<Link to={"/detail/" + item.shop_name} className='to-detail'>详情</Link>
-							</li>;
-						})
-					}
-				</ul>
-				<div className='di'></div>
+				<div class="swiper-container">
+				    <div class="swiper-wrapper">
+				        <div class="swiper-slide">Slide 1</div>
+				        <div class="swiper-slide">Slide 2</div>
+				        <div class="swiper-slide">Slide 3</div>
+				    </div>
+				    <!-- 如果需要分页器 -->
+				    <div class="swiper-pagination"></div>
+				    
+				    <!-- 如果需要导航按钮 -->
+				    <div class="swiper-button-prev"></div>
+				    <div class="swiper-button-next"></div>
+				    
+				    <!-- 如果需要滚动条 -->
+				    <div class="swiper-scrollbar"></div>
+				</div>
 			</div>
 		)
 	}
 }
 
-const mapStateHomeProps = (state) => {
-	return {
-		home_list : state.home_list
-	}
-}
 
-const mapDispatchHomeProps = (dispatch) => {
-	return {
-		fetchListData: () => {
-			fetch('/api/getdata').then((res)=>{
-				return res.json();
-			}).then((json)=>{
-				dispatch({
-					type: 'GET_HOME_LIST',
-					payload: json
-				})
-			})
-		}
-	}
-}
 
-const Home = connect( mapStateHomeProps, mapDispatchHomeProps )(HomeUI)
-
-export default Home;
+export default Home;	
