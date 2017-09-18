69d14689dbfb21dcfe06b9e0fd50d46a825dd469
zijing <1157292457@qq.com>
2017-09-18 09:31:09 +0800

    补交

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { Link } from 'react-router-dom'
 import '../homestyle.css'
-
+import Tcarousel from './home/Topcarousel'
 
 class Home extends React.Component {	
 	render() {
 		return (
 			<div>
-				
+				<Tcarousel/>
 			</div>
 		)
-	}
-		    
+	}		    
 }
 
 
---
+++ src/components/home/Topcarousel.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import { Carousel } from 'antd'
+
+class Tcarousel extends React.Component {	
+	render() {
+		return (
+			<Carousel autoplay>
+			    <div><h3>
+					<div className="tc-img">
+						<img src="http://shihuo.hupucdn.com/trade/reposition/2017-09-06/a87bb2d6088fa63ef1f07b8acf4db62f.png" alt=""/>
+					</div>
+					<div className="download-info-product product-info">
+						<p>Nike Hyperdunk 2016 Low</p>
+						<p>¥499<span>5.0折</span><span>¥999</span></p>
+					</div>
+			    </h3></div>
+			    <div><h3>2</h3></div>
+			    <div><h3>3</h3></div>
+			    <div><h3>4</h3></div>
+			 </Carousel>
+		)
+	}
+		    
+}
+
+
+export default Tcarousel;	
