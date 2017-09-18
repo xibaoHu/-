6fe05cce7c9810312ae7f8e14d842d05107db901
zijing <1157292457@qq.com>
2017-09-18 14:42:20 +0800

    首页 写到hot

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import '../homestyle.css'
+import Homesearch from './home/Homesearch'
+import Homecarousel from './home/Homecarousel'
+import PicBanner from './home/PicBanner'
 
 class Home extends Component {
 	constructor(){
@@ -8,7 +11,10 @@ class Home extends Component {
 	}
 	render() {
 		return (
-			<div>
+			<div className="home-page">
+				<Homesearch/>
+				<Homecarousel/>
+				<PicBanner/>
 			</div>
 		)
 	}		    
---
+++ src/components/home/PicBanner.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+class PicBanner extends Component {
+	render() {
+		return (
+			<div>	
+			<ul className="picbanner clearfix">
+				<li className="clearfix">
+					<div className="tit">
+						<span>篮球</span>
+						<p className="subtit">90秒乔32</p>
+					</div>
+					<div className="pic">
+						<img src="http://shihuoproxy.hupucdn.com/aHR0cDovL2dkMS5hbGljZG4uY29tL2Jhby91cGxvYWRlZC9pNS9UQjF1OUZNT3BYWFhYWE9YRlhYTDV0RDhGWFhfMDI1NDQ2LmpwZw?imageView2/0/w/160/h/160/interlace/1%7cimageMogr2/format/jpg" alt=""/>
+					</div>
+				</li>
+				<li className="clearfix">
+					<div className="tit">
+						<span>跑步</span>
+						<p className="subtit">跑步特卖场19</p>
+					</div>
+					<div className="pic">
+						<img src="http://shihuoproxy.hupucdn.com/aHR0cDovL2dkMi5hbGljZG4uY29tL2Jhby91cGxvYWRlZC9pMi9UQjFMSU8yUFZYWFhYYy5YRlhYWkxDazhWWFhfMDMyMDQzLmpwZw?imageView2/0/w/160/h/160/interlace/1%7cimageMogr2/format/jpg" alt=""/>
+					</div>
+				</li>
+				<li className="clearfix">
+					<div className="tit">
+						<span>潮流</span>
+						<p className="subtit">原创品牌优选</p>
+					</div>
+					<div className="pic">
+						<img src="http://shihuoproxy.hupucdn.com/aHR0cDovL2ltZy5hbGljZG4uY29tL2ltZ2V4dHJhL2kzLzcwMTA3MzY3Ny9UQjJFeE9Hd1I0bHB1Rmp5MXpqWFhjQUtwWGFfISE3MDEwNzM2NzcuanBn?imageView2/0/w/160/h/160/interlace/1%7cimageMogr2/format/jpg" alt=""/>
+					</div>
+				</li>
+				<li className="clearfix">
+					<div className="tit">
+						<span>健身</span>
+						<p className="subtit">循环训练别具一格</p>
+					</div>
+					<div className="pic">
+						<img src="http://shihuoproxy.hupucdn.com/aHR0cDovL3NoaWh1by5odXB1Y2RuLmNvbS9nb29kcy8yMDE3MDcvMTkyMS9lYzc0ZDBlNzdlYjEzNWNlODMyOGRiYTFhMzIyYmFkMS5qcGc?imageView2/0/w/160/h/160/interlace/1%7cimageMogr2/format/jpg" alt=""/>
+					</div>
+				</li>
+				<li className="clearfix">
+					<div className="tit">
+						<span>数码</span>
+						<p className="subtit">乔布斯式苹果</p>
+					</div>
+					<div className="pic">
+						<img src="http://shihuo.hupucdn.com/trade/reposition/2017-09-14/8b168e23f8466c55024b981534543df9.png?imageView2/0/w/160/h/160/interlace/1%7cimageMogr2/format/jpg" alt=""/>
+					</div>
+				</li>
+				<li className="clearfix">
+					<div className="tit">
+						<span>足球</span>
+						<p className="subtit">实战好像没那么贵</p>
+					</div>
+					<div className="pic">
+						<img src="http://shihuo.hupucdn.com/trade/reposition/2017-09-13/b7977b84ade0b48b25bbe48721efb324.png?imageView2/0/w/160/h/160/interlace/1%7cimageMogr2/format/jpg" alt=""/>
+					</div>
+				</li>
+			</ul>
+			<ul className="hot clearfix">
+				<li>
+					<h2>今日优惠</h2>
+					<p className="desc">游戏耳机169</p>
+					<p className="imgs">
+						<img src="http://shihuo.hupucdn.com/appHome/201709/1810/7d5978e318af435c823e6e85b15d8aed.jpg?imageView2/2/w/120/h/120/interlace/1" alt="今日优惠"/>
+					</p>
+				</li>
+				<li>
+					<h2>限时团购</h2>
+					<p className="desc">Bounce球裤新低</p>
+					<p className="imgs">
+						<img src="http://shihuo.hupucdn.com/appHome/201709/1809/3c2dc7f0a95b9f8b6362f4e94aaff6d8.jpg?imageView2/2/w/120/h/120/interlace/1" alt="限时团购"/>
+					</p>
+				</li>
+				<li>
+					<h2>一键海淘</h2>
+					<p className="desc">库里3低帮 540元</p>
+					<p className="imgs">
+						<img src="http://shihuo.hupucdn.com/appHome/201709/1511/ae7aab55bc44e6ecdeb7364b7eac6d97.jpg?imageView2/2/w/120/h/120/interlace/1" alt="一键海淘"/>
+					</p>
+				</li>
+			</ul>
+			</div>
+		)
+	}		    
+}
+
+
+
+export default PicBanner;	
