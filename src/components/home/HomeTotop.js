41ce1a0d37a025e198009af64334b9882dd51444
xibao0417 <972109653@qq.com>
2017-09-19 18:11:45 +0800

    Home toTop over

--- src/components/home/HomeTotop.js
+++ src/components/home/HomeTotop.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 
 class HomeTotop extends Component {
-	
+	constructor(){
+		super();
+		this.state = { 
+			toTop: 'none'
+		};
+	}
+	componentDidMount(){
+		var body = document.getElementsByTagName('body')[0];
+		var that = this;
+		body.addEventListener("touchmove", function(e) {
+			//console.log( that.state.toTop );
+			if( this.scrollTop >= 50 ){
+				that.setState({
+					toTop: 'block'
+				})
+			}else if( this.scrollTop <= 50 ){
+				that.setState({
+					toTop: 'none'
+				})
+			}
+		})
+	}
 	render(){
 		return(
-			<a className="totop" href="javascript:scroll(0,0)">
+			<a className="totop" href="javascript:scroll(0,0)" style={{display:this.state.toTop}}>
 				<img src="//sh1.hoopchina.com.cn/images/trademobile/daigou/top.png"/>	
 			</a>
 		)
